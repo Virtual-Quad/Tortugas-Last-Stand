fd24e6d97937e61b7912469c830d9201d07dbc5d
JRSiwiecki <JRSiwiecki@gmail.com>
2025-08-09 11:21:38 -0700

    Update cannon ball to use on screen notifier, double speed of ball

--- scripts/cannon_ball.gd
+++ scripts/cannon_ball.gd
@@ -2,16 +2,11 @@ extends Node2D
 class_name CannonBall
 
 @export var cannon_ball_sprite: AnimatedSprite2D
-@export var life_timer: Timer
+@export var on_screen_notifier: VisibleOnScreenNotifier2D
 
-@export var speed: float = 1000.0
-
-@onready var life_time_seconds: float = 5
+@export var speed: float = 2000.0
 
 var direction: Vector2 = Vector2.ZERO
-
-func _ready():
-	life_timer.start(life_time_seconds)
 
 func _process(delta: float) -> void:
 	position += direction * delta * speed
@@ -19,6 +14,7 @@ func _process(delta: float) -> void:
 func set_direction(cursor_vector: Vector2) -> void:
 	direction = cursor_vector
 
-func _on_life_timer_timeout() -> void:
-	Log.info("Cannon ball freed after " + str(life_time_seconds) + " seconds.")
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	Log.debug("Cannon ball freed after exiting screen.")
 	queue_free()
